4b0950a22e247ee1c6eccefd49fc1182c3f9ce6a
Nolwazi Masondo <masondop96@gmail.com>
2025-07-13 22:15:42 +0200

    Created specific components in React app

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function appFooter() {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default appFooter;
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function appHeader() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+export default appHeader;
