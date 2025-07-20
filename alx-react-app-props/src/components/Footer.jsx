3870e0fb92da5aa637188cad45258845bab5a2a1
Nolwazi Masondo <masondop96@gmail.com>
2025-07-20 14:37:55 +0200

    Refactored Prop Drilling to use Context API

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        border: "1px solid grey",
+        margin: "100px 10px 10px 10px",
+        backgroundColor: "lightgrey",
+      }}
+    >
+      <p>
+        © 2023 <span style={{ color: "blue" }}>City Lovers</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ color: "blue", cursor: "pointer" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
