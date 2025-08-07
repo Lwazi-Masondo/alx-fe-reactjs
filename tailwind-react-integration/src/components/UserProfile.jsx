75bf14d0942103cab2ad8da656963914e1e91d05
Nolwazi Masondo <masondop96@gmail.com>
2025-08-07 17:37:16 +0200

    Added responsive design on UserProfile card using tailwind css

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile  bg-gray-100 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg  sm:p-4 ">
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR91pnfC2-2n7skAx2c1IC8ljphj8OxC-I27AZJRNYcf9r8pQGDOhZAG_6IY_ShTPhyVbo&usqp=CAU"
         alt="User"
-        class="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24"
       />
-      <h1 class="text-xl text-blue-800 my-4">John Doe</h1>
-      <p class="text-gray-600 text-base">
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 md:text-base sm:text-sm ">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
