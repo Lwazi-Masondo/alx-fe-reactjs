237a86593ab4d275874410ed58ec5e556170d58b
Nolwazi Masondo <masondop96@gmail.com>
2025-08-07 17:45:11 +0200

    Added interactivity and transitions with Taiwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile  bg-gray-100 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg  sm:p-4 ">
+    <div className="user-profile  bg-gray-100 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg  sm:p-4 hover:shadow-xl">
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR91pnfC2-2n7skAx2c1IC8ljphj8OxC-I27AZJRNYcf9r8pQGDOhZAG_6IY_ShTPhyVbo&usqp=CAU"
         alt="User"
-        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out "
       />
-      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
+      <h1 className="md:text-xl sm:text-lg text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 md:text-base sm:text-sm ">
         Developer at Example Co. Loves to write code and explore new
         technologies.
