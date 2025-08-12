432571447b0bca6bf2b6bb4472cbb0762f8b68b9
Nolwazi Masondo <masondop96@gmail.com>
2025-08-12 02:39:21 +0200

    Created new React App for recipe sharing platform and installed TailwindCSS

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
+      <h1 className="text-blue-500">New React App</h1>
     </>
   );
 }
