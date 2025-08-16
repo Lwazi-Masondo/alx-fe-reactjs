8b5d11c01c58cc412f1f33d2385615ef7793ebed
Nolwazi Masondo <masondop96@gmail.com>
2025-08-16 18:03:51 +0200

    Added TailwindCSS styling to RecipeDetail page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -25,19 +25,23 @@ function RecipeDetail() {
 
   return (
     <div>
-      <h2>{recipe.title}</h2>
+      <h2 className="text-center my-10 font-bold text-2xl">{recipe.title}</h2>
 
-      <img src={recipe.image} alt={recipe.title} />
+      <img
+        src={recipe.image}
+        alt={recipe.title}
+        className="w-64 h-96 mx-auto md:w-96 rounded-md shadow-2xl"
+      />
 
-      <h3>ingredients</h3>
-      <ol>
+      <h3 className="text-center font-bold my-20 text-xl">Ingredients</h3>
+      <ol className="text-center mb-20 text-base">
         {recipe.ingredients.map((ingrediant, index) => (
           <li key={index}>{ingrediant}</li>
         ))}
       </ol>
 
-      <h3>Instructions</h3>
-      <ol>
+      <h3 className="text-center font-bold mb-16 text-xl">Instructions</h3>
+      <ol className="mx-20 text-center mb-20 text-base">
         <li>{recipe.instructions}</li>
       </ol>
     </div>
