cf9a2ef40321acb19a0c79f74042677ff5978d02
Nolwazi Masondo <masondop96@gmail.com>
2025-09-06 18:26:17 +0200

    Prepared router integration

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+
+function App() {
+  return (
+    <>
+      <h1>Advanced Routing in React with React Router</h1>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
