fb9fef8daa22dd9af148cdb633a34914d2044d58
Nolwazi Masondo <masondop96@gmail.com>
2025-09-06 14:03:48 +0200

    Fetched data from JSONPlaceholder API

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl text-center text-blue-500">Heading</h1>
+      <h1 className="text-3xl text-center text-blue-500">Heading</h1>
       <QueryClientProvider client={queryClient}>
         <PostsComponent />
       </QueryClientProvider>
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from "react-query";
+
+const fetchData = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!response.ok) throw new Error("Network response was not ok");
+  return response.json();
+};
+
+const PostsComponent = () => {
+  //Use the useQuery hook to handle data fetching and caching
+  const { data, error, isLoading } = useQuery("fetchData", fetchData);
+
+  //Handle loading state
+  if (isLoading) return <div className="text-green-500 ">Loading...</div>;
+
+  //Handle error state
+  if (error) return <div>Error loading data</div>;
+
+  //Render the fetched data
+  return (
+    <div>
+      {data.map((post) => (
+        <div key={post.id} className="my-10 border-2 border-gray-600">
+          {post.title}
+          <p key={post.id}>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsComponent;
